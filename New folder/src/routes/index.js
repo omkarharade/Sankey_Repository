f815b6a6a52e4b4973eb99cee3e6cfac52fc9769
Omkar Harade <omkarharade1301@gmail.com>
2023-07-27 04:51:37 +0530

    added apis and CRUD operations

--- New folder/src/routes/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    HeroRoutes : require("./hero-routes")
-}
--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,12 +2,12 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-module.exports = {
-    USER : process.env.USER,
-    HOST : process.env.HOST,
-    DATABASE : process.env.DATABASE,
-    PASSWORD : process.env.PASSWORD,
-    DIALECT : process.env.DIALECT,
-    DB_PORT : process.env.DB_PORT,
-    SERVER_PORT : process.env.SERVER_PORT,
-};
+// module.exports = {
+//     USER : process.env.USER,
+//     HOST : process.env.HOST,
+//     DATABASE : process.env.DATABASE,
+//     PASSWORD : process.env.PASSWORD,
+//     DIALECT : process.env.DIALECT,
+//     DB_PORT : process.env.DB_PORT,
+//     SERVER_PORT : process.env.SERVER_PORT,
+// };
